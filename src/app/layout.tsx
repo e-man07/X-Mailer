4cdea1f4ad3e41ddcfbbb754d14fc832d5c6b629
e-man07 <amann.jha1107@gmail.com>
2025-08-20 03:36:38 +0530

    added meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="solads-site-verification" content="solads_verify_1755640973371_d61b3012c7dad6a1e0203da965374c97" />
+        <meta name="solads-site-verification" content="solads_verify_1755640973371_d61b3012c7dad6a1e0203da965374c95" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
